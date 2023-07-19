32484c555f71304d0d9f1d4b08054baee6ed5059
[SW5]최형욱 <guddnr888@gmail.com>
2023-07-19 21:29:38 +0900

    "React:useState 강의"

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import "./App.css";
+type counterProps = {
+    title: string;
+    initValue: number;
+};
+function Counter({ title, initValue }: counterProps) {
+    // let countState = useState(initValue);
+    // let count = countState[0]
+    // let setCount = countState[1];
+    const [count, setCount] = useState(initValue)
+    return (
+        <div>
+            <h1>{title}</h1>
+            <button
+                onClick={() => {
+                setCount(count+1)}}
+            >
+                +
+            </button>
+            {count}
+        </div>
+    );
+}
 
 function App() {
-  return (
-    <div>
-     hi
-    </div>
-  );
+    return (
+        <div>
+            <Counter title="카운터" initValue={10}></Counter>
+        </div>
+    );
 }
 
 export default App;
